f337bcd3e66e85245413ce9cc2d4d75b9e5256e2
Renée Kooi <renee@kooi.me>
2018-05-15 10:35:04 +0200

    Update material-ui to v1.0.0-rc.0.

--- src/DescriptionDialog.js
+++ src/DescriptionDialog.js
@@ -2,14 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import stripIndent from 'strip-indent';
 import Markdown from 'react-markdown';
-import Button from 'material-ui/Button';
-import MuiDialog, {
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-  withMobileDialog,
-} from 'material-ui/Dialog';
-import { withStyles } from 'material-ui/styles';
+import Button from '@material-ui/core/Button';
+import MuiDialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogActions from '@material-ui/core/DialogActions';
+import withMobileDialog from '@material-ui/core/withMobileDialog';
+import { withStyles } from '@material-ui/core/styles';
 
 const Dialog = withMobileDialog()(MuiDialog);
 
